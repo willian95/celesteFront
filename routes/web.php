a03c81e5db338fc3c2d2faefbe9f128decbee02f
willian95 <rodriguezwillian95@gmail.com>
2021-09-03 10:03:11 -0400

    comming soon

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('comingsoon');
+});
+
+Route::get('/home', function () {
     return view('welcome');
 });
 
